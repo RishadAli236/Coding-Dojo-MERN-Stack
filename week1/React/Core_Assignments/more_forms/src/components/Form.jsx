1d172626c1ad125454227e47c9f5d18f6fc7956f
Rishad Yasin <rishad236@gmail.com>
2023-07-20 17:37:38 -0700

    optimized with less pieces of state

--- week1/React/Core_Assignments/more_forms/src/components/Form.jsx
+++ week1/React/Core_Assignments/more_forms/src/components/Form.jsx
@@ -7,103 +7,52 @@ function Form () {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [firstNameErr, setFirstNameErr] = useState("");
-    const [lastNameErr, setLastNameErr] = useState("")
-    const [emailErr, setEmailErr] = useState("")
-    const [passwordErr, setPasswordErr] = useState("")
-    const [confirmPasswordErr, setConfirmPasswordErr] = useState("")
-
-    const handleFirstName = (e) => {
-        setFirstName(e.target.value);
-        if (e.target.value.length > 0 && e.target.value.length < 2){
-            setFirstNameErr("First Name should be at least 2 characters");
-        }
-        else{
-            setFirstNameErr("");
-        }
-    }
-    const handleLastName = (e) => {
-        setLastName(e.target.value);
-        if (e.target.value.length > 0 && e.target.value.length < 2){
-            setLastNameErr("Last Name should be at least 2 characters");
-        }
-        else{
-            setLastNameErr("");
-        }
-    }
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-        if (e.target.value.length > 0 && e.target.value.length < 5){
-            setEmailErr("Email should be at least 5 characters");
-        }
-        else{
-            setEmailErr("");
-        }
-    }
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-        if (e.target.value.length > 0 && e.target.value.length < 8){
-            setPasswordErr("Password must be at least 8 characters");
-        }
-        else{
-            setPasswordErr("");
-        }
-    }
-    const handleConfirmPassword= (e) => {
-        setConfirmPassword(e.target.value);
-        if (e.target.value.length > 0 && e.target.value != password){
-            setConfirmPasswordErr("Passwords must match");
-        }
-        else{
-            setConfirmPasswordErr("");
-        }
-    }
 
     return (
         <div>
             <form>
                 <div>
                     <label htmlFor="firstName">First Name</label>
-                    <input type="text" onChange={handleFirstName}/>
+                    <input type="text" onChange={(e) => setFirstName(e.target.value)}/>
                     {
-                        firstNameErr?
-                        <p>{firstNameErr}</p>:
+                        firstName.length > 0 && firstName.length < 2?
+                        <p>First Name should be at least 2 characters</p>:
                         ""
                     }
                 </div>
                 <div>
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" onChange={handleLastName}/>
+                    <input type="text" onChange={(e) => setLastName(e.target.value)}/>
                     {
-                        lastNameErr?
-                        <p>{lastNameErr}</p>:
+                        lastName.length > 0 && lastName.length < 2?
+                        <p>Last Name should be at least 2 characters</p>:
                         ""
                     }
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="text" onChange={handleEmail}/>
+                    <input type="email" onChange={(e) => setEmail(e.target.value)}/>
                     {
-                        emailErr?
-                        <p>{emailErr}</p>:
+                        email.length > 0 && email.length < 5?
+                        <p>Email should be at least 5 characters</p>:
                         ""
                     }
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="password" onChange={handlePassword}/>
+                    <input type="password" onChange={(e) => setPassword(e.target.value)}/>
                     {
-                        passwordErr?
-                        <p>{passwordErr}</p>:
+                        password.length > 0 && password.length < 8?
+                        <p>Password must be at least 8 characters</p>:
                         ""
                     }
                 </div>
                 <div>
                     <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password" onChange={handleConfirmPassword}/>
+                    <input type="password" onChange={(e) => setConfirmPassword(e.target.value)}/>
                     {
-                        confirmPasswordErr?
-                        <p>{confirmPasswordErr}</p>:
+                        confirmPassword.length > 0 && confirmPassword !== password?
+                        <p>Passwords must match</p>:
                         ""
                     }
                 </div>
